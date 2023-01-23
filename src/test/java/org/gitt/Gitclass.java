2e2bcb2a33795548495a36242c44823daba02b18
Kaviraj143 <kingkaviraj3@gmail.com>
2023-01-23 08:49:44 +0530

    First time push

---
+++ src/test/java/org/gitt/Gitclass.java
@@ -0,0 +1,16 @@
+package org.gitt;
+
+public class Gitclass {
+	
+	private void method1() {
+		System.out.println("Kaviraj");
+ 
+	}
+	private void method2() {
+		System.out.println("guru");
+	}
+	private void method3() {
+		System.out.println("king");
+	}
+
+}
